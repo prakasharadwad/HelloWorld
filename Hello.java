2c432775d4a99cbcc971561478e72a8b15dd8bfd
prakasharadwad <prakasharadwad@gmail.com>
2017-05-30 14:43:16 -0300

    Update Hello.java
    test

--- Hello.java
+++ Hello.java
@@ -4,6 +4,8 @@ public class Hello {
 	
 	public static void main(String[]args){
 		for(int i=0;i<=10;i++){
+			
+			System.out.println("Welcome to java:"+i);
 			System.out.println("Welcome to java:"+i);
 		}
 		
